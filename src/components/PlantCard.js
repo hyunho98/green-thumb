4b813931a1c56ddd3763ac1273beb6d7fb1b31a3
Alex Lee <hyunho98@gmail.com>
2023-04-06 13:23:03 -0400

    cards now show relevant information

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,8 +1,15 @@
 import "../App.css"
-import React from "react"
+import React, { useState } from "react"
 import { Card } from "semantic-ui-react"
 
-function PlantCard({ image, name, type }) {
+function PlantCard({ image, name, type, alert, bloom, timer }) {
+    const [bloomDate, setBloomDate] = useState(new Date(bloom))
+    const timeLeft = (bloomDate.getTime() - timer) / 1000
+    const hours = Math.floor((timeLeft / 3600))
+    const minutes = Math.floor((timeLeft % (3600)) / (60))
+    const seconds = Math.floor((timeLeft % (60)))
+
+    const untilBloom = ""
     return (
         <Card>
             <div>
@@ -12,6 +19,10 @@ function PlantCard({ image, name, type }) {
                 <div className="content">
                     <h3 className="plantName">{name}</h3>
                     <h4 className="plantType">{type.toUpperCase()}</h4>
+                    <h4 className="plantAlert">Time to Water</h4>
+                    <h4 className="plantAlert">{hours + "h " + minutes + "m " + seconds + "s"}</h4>
+                    <h4 className="plantBloom"> Bloom Date</h4>
+                    <h4 className="plantBloom">{`${bloomDate.getMonth() + 1}-${bloomDate.getDay()}-${bloomDate.getFullYear()}`}</h4>
                 </div>
             </div>
         </Card>
--- src/components/PlantContainer.js
+++ src/components/PlantContainer.js
@@ -1,14 +1,28 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Card, Container } from "semantic-ui-react"
 import PlantCard from "./PlantCard"
 
 function PlantContainer({ plants }) {
+    const [timer, setTimer] = useState(Date.now())
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimer(Date.now())
+        }, 1000)
+
+        return (() => clearInterval(interval))
+    }, [timer])
+
     const plantList = plants.map((plant) => 
         <PlantCard 
             key={plant.id}
             image={plant.image}
-            name={plant.name} 
-            type={plant.type} 
+            name={plant.name}
+            alert={plant["alert-time"]}
+            bloom={plant["bloom-date"]}
+            type={plant.type}
+            timer={timer}
+            
         />
     )
 
