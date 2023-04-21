333011fc64d58b6b27c34322f35ef379ed2c8c58
Alex Lee <hyunho98@gmail.com>
2023-04-21 14:11:21 -0400

    made form controlled

--- src/components/PlantForm.js
+++ src/components/PlantForm.js
@@ -12,23 +12,23 @@ function PlantForm({ handleFormSubmit, plant=null, deletePlant=null }) {
         Delete Plant
     </Button>
 
-    const imageHolder = plant ? plant.image : "Image Url"
-    const nameHolder = plant ? plant.name : "Plant Name"
-    const typeHolder = plant ? plant.type : "Plant Type"
-    const hourHolder = plant ? (plant.alert.time / 3600000) + " Hours" : "Hours"
-    const dateHolder = plant ? plant["bloom-date"] : new Date().toISOString().slice(0,10)
+    const [imageHold, setImageHold] = useState(plant ? plant.image : null)
+    const [nameHold, setNameHold] = useState(plant ? plant.name : null)
+    const [typeHold, setTypeHold] = useState(plant ? plant.type : null)
+    const [hourHold, setHourHold] = useState(plant ? (plant.alert.time / 3600000) : null)
+    const dateHold = plant ? plant["bloom-date"] : new Date().toISOString().slice(0,10)
 
     function submitHandler(e) {
         e.preventDefault()
         const fetchBody = {
-            "name": e.target.name.value ? e.target.name.value : nameHolder,
-            "type": e.target.type.value ? e.target.type.value : typeHolder,
-            "image": e.target.image.value ? e.target.image.value : (plant ? plant.image : "https://art.pixilart.com/bd06a316a4ad6b2.png"),
+            "name": nameHold ? nameHold : "Plant Name",
+            "type": typeHold ? typeHold : "Plant Type",
+            "image": imageHold ? imageHold : (plant ? plant.image : "https://art.pixilart.com/bd06a316a4ad6b2.png"),
             "alert": {
-                "time": e.target.timer.value ? parseInt(e.target.timer.value) * 3600000 : (plant ? plant.alert.time : 0),
-                "date": e.target.timer.value ? (e.target.timer.value * 3600000) + Date.now() : (plant ? plant.alert.date : Date.now())
+                "time": hourHold ? parseInt(hourHold) * 3600000 : (plant ? plant.alert.time : 0),
+                "date": hourHold ? (hourHold * 3600000) + Date.now() : (plant ? plant.alert.date : Date.now())
             },
-            "bloom-date": e.target.date.value ? e.target.date.value : dateHolder
+            "bloom-date": e.target.date.value ? e.target.date.value : dateHold
         }
 
         if(plant) {
@@ -81,18 +81,34 @@ function PlantForm({ handleFormSubmit, plant=null, deletePlant=null }) {
           {welcomeMessage}
           <Form className="plantForm" onSubmit={submitHandler} >
                 <Form.Input
+                    onChange={(e) => setImageHold(e.target.value)}
                     label="Plant Image url"
-                    placeholder={imageHolder}
+                    placeholder="Image Url"
                     name="image"
+                    value={imageHold}
                 />
-                <Form.Input label="Plant Name" placeholder={nameHolder} name="name" />
-                <Form.Input label="Plant Type" placeholder={typeHolder} name="type" />
                 <Form.Input
-                label="Timer"
-                type="number"
-                placeholder={hourHolder}
-                min={0}
-                name="timer"
+                    onChange={(e) => setNameHold(e.target.value)}
+                    label="Plant Name" 
+                    placeholder="Plant Name" 
+                    name="name" 
+                    value={nameHold}
+                />
+                <Form.Input 
+                    onChange={(e) => setTypeHold(e.target.value)}
+                    label="Plant Type" 
+                    placeholder="Plant Type" 
+                    name="type" 
+                    value={typeHold}
+                />
+                <Form.Input
+                    onChange={(e) => setHourHold(e.target.value)}
+                    label="Timer (Hours)"
+                    type="number"
+                    placeholder="Hours"
+                    min={0}
+                    name="timer"
+                    value={hourHold}
                 />
                 <Form.Input
                     label="Bloom Date" 
